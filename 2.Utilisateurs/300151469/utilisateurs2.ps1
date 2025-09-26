a7b215837a849b1fa81500fae6e64bdd7cdfe199
rabia <rabiabouhali01@gmail.com>
2025-09-25 20:18:48 -0400

    voila mon fichier README.md -300151469 merci

---
+++ 2.Utilisateurs/300151469/utilisateurs2.ps1
@@ -0,0 +1,9 @@
+# Créer des groupes
+$Groups = @{
+    "GroupeFormation" = @()
+    "ProfesseursAD" = @()
+}
+
+# Ajouter un utilisateur à un groupe
+$Groups["GroupeFormation"] += $Users[0]   # Alice Dupont
+
