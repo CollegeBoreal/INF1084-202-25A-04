57725c8e475e81ba1ef58944f64779d0833b0948
brobert <brobert@valiha.com>
2025-09-26 12:29:01 -0400

    .

--- 2.Utilisateurs/.scripts/participation.sh
+++ 2.Utilisateurs/.scripts/participation.sh
@@ -30,8 +30,8 @@ echo "| :x:                | Projet inexistant             |"
 echo ""
 echo "## :a: Présence"
 echo ""
-echo "|:hash:| Boréal :id:                | README.md    | :one: | :two: |"
-echo "|------|----------------------------|--------------|-------|-------"
+echo "|:hash:| Boréal :id:                | README.md    | :one: | :two: | :three: | :four: |"
+echo "|------|----------------------------|--------------|-------|-------|---------|--------|"
 
 i=0
 s=0
@@ -42,7 +42,9 @@ do
    FILE=${id}/README.md
    USR1=${id}/utilisateurs1.ps1; OK_USR1=":x:"
    USR2=${id}/utilisateurs2.ps1; OK_USR2=":x:"
-   KO="| ${i} | [${id}](../${FILE}) :point_right: ${URL} | :x: | :x: | :x: |"
+   USR3=${id}/utilisateurs3.ps1; OK_USR3=":x:"
+   USR4=${id}/utilisateurs4.ps1; OK_USR4=":x:"
+   KO="| ${i} | [${id}](../${FILE}) :point_right: ${URL} | :x: | :x: | :x: | :x: | :x: |"
    if [ -f "$FILE" ]; then
     ACTUAL_NAME="$(basename "$(realpath "$FILE")")"
     if [[ "$ACTUAL_NAME" == "README.md" ]]; then
@@ -52,7 +54,13 @@ do
         if [ -f "$USR2" ]; then
                 OK_USR2=":heavy_check_mark:"
         fi
-        FULL_OK="| ${i} | [${id}](../${FILE}) :point_right: ${URL} | :heavy_check_mark:| ${OK_USR1} | ${OK_USR2} | "
+        if [ -f "$USR3" ]; then
+                OK_USR3=":heavy_check_mark:"
+        fi
+        if [ -f "$USR4" ]; then
+                OK_USR4=":heavy_check_mark:"
+        fi
+        FULL_OK="| ${i} | [${id}](../${FILE}) :point_right: ${URL} | :heavy_check_mark:| ${OK_USR1} | ${OK_USR2} | ${OK_USR3} | ${OK_USR4} | "
         echo ${FULL_OK}
         let "s++"
     else
