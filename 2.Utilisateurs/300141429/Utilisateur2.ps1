90ed1cbad0739f2090c3761fce55883b559107e0
Barrynetwork <barryarona2003@gmail.com>
2025-09-30 19:08:44 -0400

    Ajout des scripts utilisateurs et README

---
+++ 2.Utilisateurs/300141429/Utilisateur2.ps1
@@ -0,0 +1,11 @@
+# Créer une liste d'utilisateurs simulés
+$Users = @(
+    @{Nom="Dupont"; Prenom="Alice"; Login="adupont"; OU="Stagiaires"},
+    @{Nom="Lemoine"; Prenom="Sarah"; Login="slemoine"; OU="Stagiaires"},
+    @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"}
+@{Nom="Ndiaye"; Prenom="Oumar"; Login="ndiaye"; OU="Stagiaires>    
+@{Nom="DIONE"; Prenom="Fatou"; Login="dione"; OU="Stagiaires>    
+)
+
+# Afficher les utilisateurs
+$Users | ForEach-Object { "$($_.Prenom) $($_.Nom) - Login: $($_.Login) - OU: $($_.OU)" }
