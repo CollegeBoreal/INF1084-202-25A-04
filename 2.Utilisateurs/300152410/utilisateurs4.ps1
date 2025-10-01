4ca2a50982aa8eb08fa2ca3d840fd4f077874f6d
imadboudeuf <damiboudeuf@gmail.com>
2025-10-01 03:51:17 +0200

    Ajout des scripts utilisateurs 1-4 et UsersSimules.csv (Imad Boudeuf 300152410)

--- 2.Utilisateurs/300152410/utilisateurs4.ps1
+++ 2.Utilisateurs/300152410/utilisateurs4.ps1
@@ -2,7 +2,8 @@
 # Exercice 4 : Importer le fichier CSV et créer un groupe ImportGroupe avec les utilisateurs
 
 # Importer les utilisateurs depuis le fichier CSV (dans le même dossier que le script)
-$ImportedUsers = Import-Csv -Path ".\UsersSimules.csv"
+$ImportedUsers = Import-Csv -Path "C:\Users\microstar\INF1084-202-25A-04\2.Utilisateurs\300152410\UsersSimules.csv"
+
 
 # Créer un groupe ImportGroupe
 $Groups = @{
