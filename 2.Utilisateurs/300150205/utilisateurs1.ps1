57fe468426227b2216759b610c48d306cc5895f1
blbsblm <boualembelbessai@gmail.com>
2025-10-01 16:59:31 -0400

    atelier 2-utilisateurs

--- 2.Utilisateurs/300150205/utilisateurs1.ps1
+++ 2.Utilisateurs/300150205/utilisateurs1.ps1
@@ -3,7 +3,8 @@ $Users = @(
     @{Nom="Dupont"; Prenom="Alice"; Login="adupont"; OU="Stagiaires"},
     @{Nom="Lemoine"; Prenom="Sarah"; Login="slemoine"; OU="Stagiaires"},
     @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"},
-    @{Nom="Ngue"; Prenom="Taylor"; Login="tngue"; OU="Etudiants"}
+    @{Nom="Martin"; Prenom="Sophie"; Login="MSophie"; OU="Etudiants"},
+    @{Nom="Dubois"; Prenom="Lucas"; Login="DLucas"; OU="Stagiaires"}
 )
 
 # Afficher les utilisateurs
