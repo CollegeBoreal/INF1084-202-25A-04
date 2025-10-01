19c4ec823e747343055536c0b55fa339d4c812e6
ismailtrache <ismailtrache2@gmail.com>
2025-10-01 01:25:41 -0400

    A

--- 2.Utilisateurs/300150395/utilisateurs4.ps1
+++ 2.Utilisateurs/300150395/utilisateurs4.ps1
@@ -1,21 +1,17 @@
 # utilisateurs4.ps1
-# Mini-projet : création d'utilisateurs et groupe
+# Exercice 4 : Importer le fichier CSV et créer un groupe ImportGroupe avec les utilisateurs
 
-# Charger utilisateurs1.ps1 pour récupérer les utilisateurs
-. .\utilisateurs1.ps1
+# Importer les utilisateurs depuis le fichier CSV (dans le même dossier que le script)
+$ImportedUsers = Import-Csv -Path ".\UsersSimules.csv"
 
-# Créer un groupe "Etudiants2025"
+# Créer un groupe ImportGroupe
 $Groups = @{
-    "Etudiants2025" = @()
+    "ImportGroupe" = @()
 }
 
-# Ajouter tous les utilisateurs de "Promo2025" dans le groupe
-$Groups["Etudiants2025"] += $Users | Where-Object {$_.OU -eq "Promo2025"}
+# Ajouter tous les utilisateurs importés dans ImportGroupe
+$Groups["ImportGroupe"] += $ImportedUsers
 
-# Exporter la liste finale en CSV
-$Groups["Etudiants2025"] | Export-Csv -Path "C:\Temp\Etudiants2025.csv" -NoTypeInformation
-
-# Afficher les utilisateurs
-"Utilisateurs dans Etudiants2025 :"
-$Groups["Etudiants2025"] | ForEach-Object { "$($_.Prenom) $($_.Nom)" }
-
+# Afficher les utilisateurs du groupe ImportGroupe
+"Utilisateurs dans ImportGroupe :"
+$Groups["ImportGroupe"] | ForEach-Object { "$($_.Prenom) $($_.Nom)" }
