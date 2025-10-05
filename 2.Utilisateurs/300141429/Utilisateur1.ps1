50dda8bb3e0141b8b9e787234e1e83340e050f26
Barrynetwork <barryarona2003@gmail.com>
2025-10-05 03:51:48 -0400

    Update Utilisateur1.ps1

--- 2.Utilisateurs/300141429/Utilisateur1.ps1
+++ 2.Utilisateurs/300141429/Utilisateur1.ps1
@@ -2,7 +2,7 @@
 $Users = @(
     @{Nom="Dupont"; Prenom="Alice"; Login="adupont"; OU="Stagiaires"},
     @{Nom="Lemoine"; Prenom="Sarah"; Login="slemoine"; OU="Stagiaires"},
-    @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"},
+    @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"}
 
 
 )
