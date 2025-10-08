f001e1328169dc8c1dc6902d67278726c6dddf5a
nad1111 <300150485@monboreal.ca>
2025-10-08 12:51:09 -0300

    Ajout de tous les scripts utilisateurs dans 300150485

---
+++ 2.Utilisateurs/300150485/utilisateurs1.ps1
@@ -0,0 +1,10 @@
+# utilisateurs1.ps1
+# Script PowerShell pour créer un utilisateur Active Directory
+
+# Nom et prénom de l'utilisateur
+$FirstName = "Luc"
+$LastName = "Martin"
+$SamAccountName = "lmartin"
+
+# Créer un nouvel utilisateur AD (simulation)
+Write-Host "Création de l'utilisateur : $FirstName $LastName ($SamAccountName)"
