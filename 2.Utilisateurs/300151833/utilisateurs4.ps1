0572cbdb9a232d1512ce0352c24b7ca495028333
Raoufbrs15 <109877652+Raoufbrs15@users.noreply.github.com>
2025-10-30 23:52:49 -0400

    edit

--- 2.Utilisateurs/300151833/utilisateurs4.ps1
+++ 2.Utilisateurs/300151833/utilisateurs4.ps1
@@ -1,13 +1,17 @@
+$Users = @(   
+    [PSCustomObject]@{ Nom="Dupont"; Prenom="Alice"; OU="Stagiaires" }  
+    [PSCustomObject]@{ Nom="Diaks"; Prenom="rahima"; OU="Professeurs" } 
+    [PSCustomObject]@{ Nom="malembe"; Prenom="driss"; OU="Stagiaires" }
+)
 
-$CsvPath = Join-Path -Path $PSScriptRoot -ChildPath "UsersSimules.csv"
-
-$ImportedUsers = Import-Csv -Path $CsvPath
-
-$Groups = @{}
-$Groups["ImportGroupe"] = @()
-
-foreach ($user in $ImportedUsers) {
-    $Groups["ImportGroupe"] += $user
+if (-not (Test-Path -Path "C:\Temp")) { 
+   New-Item -ItemType Directory -Path "C:\Temp" | Out-Null
 }
 
-$Groups["ImportGroupe"]
+$Users | Export-Csv -Path "C:\Temp\UsersSimules.csv" -NoTypeInformation -Encoding UTF8
+
+$ImportedUsers = Import-Csv -Path "C:\Temp\UsersSimules.csv"
+
+Write-Host "Utilisateurs importés depuis le CSV :" -ForegroundColor Cyan
+
+$ImportedUsers | Format-Table -AutoSize
