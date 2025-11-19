b0293aaaa1b242d9e17d44a6d5d4916a4feff112
zoumarbalde <zoumarbalde@gmail.com>
2025-11-18 20:11:55 -0500

    My job

---
+++ 5.Services/300147629/services1.ps1
@@ -0,0 +1,5 @@
+Get-Service | Where-Object {
+    $_.DisplayName -like "*Directory*" -or $_.Name -match "NTDS|ADWS|DFSR|kdc|Netlogon|IsmServ"
+} | Sort-Object DisplayName
+
+Get-Service -Name NTDS, ADWS, DFSR
