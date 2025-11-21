49a06d1149d23fa0ccc5c196c72fc29293773e5b
B300098957 <300098957@collegeboreal.ca>
2025-11-21 14:28:09 -0500

    .

--- 7.DCs/.scripts/participation.ps1
+++ 7.DCs/.scripts/participation.ps1
@@ -47,7 +47,7 @@ foreach ($id in $GROUPES) {
     $URL1 = "[<image src='https://avatars0.githubusercontent.com/u/$($AVATARS[$i1])?s=460&v=4' width=20 height=20></image>](https://github.com/$($IDS[$i1]))"
 
     if ($second -eq 300098957) {
-       $URL2 = "[<image src='https://avatars0.githubusercontent.com/u/62551735?s=460&v=4' width=20 height=20></image>](https://github.com/b300098957))"
+       $URL2 = "[<image src='https://avatars0.githubusercontent.com/u/62551735?s=460&v=4' width=20 height=20></image>](https://github.com/b300098957)"
     }
     else {
        $URL2 = "[<image src='https://avatars0.githubusercontent.com/u/$($AVATARS[$i2])?s=460&v=4' width=20 height=20></image>](https://github.com/$($IDS[$i2]))"
