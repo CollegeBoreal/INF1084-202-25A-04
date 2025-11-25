ee02fa6b4f19c64c50fb0534aecb1ff047855e25
hicham hamdi <62858035+hichemhamdi10@users.noreply.github.com>
2025-11-25 11:38:31 -0500

    Update utilisateurs4.ps1

--- 2.Utilisateurs/300151042/utilisateurs4.ps1
+++ 2.Utilisateurs/300151042/utilisateurs4.ps1
@@ -1,10 +1,12 @@
-# Exporter les utilisateurs simulés
-$Users | Export-Csv -Path "C:\Temp\UsersSimules.csv" -NoTypeInformation
-# Import du CSV
+ creat folder C:\Temp s'il n'existe pas
+if (-not (Test-Path -Path "C:\Temp")) { 
+   New-Item -ItemType Directory -Path "C:\Temp" | Out-Null
+}
+# Export users
+$Users | Export-Csv -Path "C:\Temp\UsersSimules.csv" -NoTypeInformation -Encoding UTF8
+# import users
 $ImportedUsers = Import-Csv -Path "C:\Temp\UsersSimules.csv"
+# Show the users
+Write-Host "Utilisateurs importés depuis le CSV :" -ForegroundColor Cyan
 
-# Création du groupe
-$Groups["ImportGroupe"] = @()
-
-# Ajout des utilisateurs importés
-$Groups["ImportGroupe"] += $ImportedUsers
+$ImportedUsers | Format-Table -AutoSize
