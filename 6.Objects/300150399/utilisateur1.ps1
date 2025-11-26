b6a64bc099a16e6fa901245457c83ab5b6090458
Chkips <chakibrahmani22@gmail.com>
2025-11-25 20:34:38 -0500

    Delete 6.Objects/300150399/utilisateur1.ps1

--- 6.Objects/300150399/utilisateur1.ps1
+++
@@ -1,8 +0,0 @@
-- Création du partage : C:\SharedResources
-- Attribution des permissions NTFS / partage
-- Création de l’OU : Students
-- Création des comptes utilisateurs :
-    - Etudiant1
-    - Etudiant2
-- Création du groupe global : Students
-- Ajout des utilisateurs Etudiant1 et Etudiant2 dans le groupe Students
