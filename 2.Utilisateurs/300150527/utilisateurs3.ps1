c4741ae672ddfe6703a3b3d9abc61d29b6f86005
Akrem Bouraoui <bouraoui.akrem.25@hotmail.com>
2025-11-28 13:45:03 -0500

    mise a jour

--- 2.Utilisateurs/300150527/utilisateurs3.ps1
+++ 2.Utilisateurs/300150527/utilisateurs3.ps1
@@ -1,16 +1,17 @@
 # utilisateurs3.ps1
-# Exercice 3 : 
+# Requêtes et filtres
 
 # Liste des utilisateurs simulés
 $Users = @(
-    @{Nom="Dupont"; Prenom="Alice"; Login="adupont"; OU="Stagiaires"},
-    @{Nom="Lemoine"; Prenom="Sarah"; Login="slemoine"; OU="Stagiaires"},
-    @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"},
-    @{Nom="Moulin"; Prenom="Jean"; Login="jmoulin"; OU="Stagiaires"},
-    @{Nom="Martin"; Prenom="Paul"; Login="pmartin"; OU="Informatique"}
+    @{Nom="Dupont";  Prenom="Alice";   Login="adupont";  OU="Stagiaires"},
+    @{Nom="Lemoine"; Prenom="Sarah";   Login="slemoine"; OU="Stagiaires"},
+    @{Nom="Benali";  Prenom="Karim";   Login="kbenali";  OU="Stagiaires"},
+    @{Nom="Bouraoui"; Prenom="Akrem";  Login="abouraoui"; OU="Stagiaires"},
+    @{Nom="Junior";   Prenom="Neymar"; Login="njunior";   OU="Stagiaires"}
 )
 
-# Filtrer et afficher les prénoms qui contiennent la lettre "a"
-$Users | Where-Object { $_.Prenom -match "a" } | ForEach-Object {
-    "$($_.Prenom) $($_.Nom)"
+# Exercice 3 : Lister tous les utilisateurs dont le prénom contient "a"
+$Users | Where-Object { $_.Prenom -match "a" } | 
+ForEach-Object {
+    "$($_.Prenom) $($_.Nom) - Login: $($_.Login)"
 }
