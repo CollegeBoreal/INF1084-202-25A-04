08b592f07207438e1315652b0f9d858ea2ef9dc1
Diallo <mamadouabdoulayediallo52@gmail.com>
2025-12-06 02:46:40 -0500

    merci

--- 4.OUs/300147786/utilisateurs2.ps1
+++ 4.OUs/300147786/utilisateurs2.ps1
@@ -1,10 +1,10 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
 Import-Module ActiveDirectory
 
 . .\bootstrap.ps1
 
 # Vérifier si l'utilisateur existe déjà
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-
 $user = Get-ADUser -Filter "SamAccountName -eq 'alice.dupont'" -Server $domainName -ErrorAction SilentlyContinue
 
 if (-not $user) {
