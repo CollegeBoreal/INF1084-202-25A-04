89b1e34a2b8231717d90c8e2581872e6fc83236a
makoudimassinissa <makoudimassimtts@gmail.com>
2025-12-06 02:05:58 -0500

    script

---
+++ 6.Objects/300151354/bootstrap.ps1
@@ -0,0 +1,33 @@
+# ==========================================
+# bootstrap.ps1
+# Initialisation du domaine et des credentials
+# ==========================================
+
+# Numéro étudiant + instance
+$studentNumber = 300151354
+$studentInstance = 0
+
+# Noms du domaine
+$domainName = "DC$studentNumber-$studentInstance.local"
+$netbiosName = "DC$studentNumber-$studentInstance"
+
+Write-Host "Domain FQDN  : $domainName"
+Write-Host "NetBIOS Name : $netbiosName"
+
+# Credentials
+$plain = "Infra@2024"
+$secure = ConvertTo-SecureString $plain -AsPlainText -Force
+
+$cred = New-Object System.Management.Automation.PSCredential(
+    "Administrator@$domainName",
+    $secure
+)
+
+# Modules
+Import-Module ActiveDirectory
+Import-Module GroupPolicy
+
+# Vérification
+Get-ADDomain -Server $domainName
+Get-ADDomainController -Filter * -Server $domainName
+
