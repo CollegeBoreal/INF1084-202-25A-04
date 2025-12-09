a8fbade1abf53e4eaa5a87ff7fa308f3037fdd51
unknown <Administrator@DC300150395-00.LOCAL>
2025-12-09 03:10:29 -0500

    modif

--- 5.Services/300150395/services1.ps1
+++ 5.Services/300150395/services1.ps1
@@ -1,14 +1,14 @@
 # services1.ps1 - Ismail Trache 300150395
-# Objectif : Lister tous les services liés à Active Directory et vérifier leur état.
+# Objectif : Lister tous les services liés a Active Directory et verifier leur etat.
 
 Write-Host "=== Liste des services Active Directory ===" -ForegroundColor Cyan
 
-# Lister tous les services liés à AD
+# Lister tous les services lies à AD
 Get-Service | Where-Object {
     $_.DisplayName -like "*Directory*" -or $_.Name -match "NTDS|ADWS|DFSR|kdc|Netlogon|IsmServ"
 } | Sort-Object DisplayName
 
-Write-Host "`n=== État des services principaux ===" -ForegroundColor Cyan
+Write-Host "`n=== Etat des services principaux ===" -ForegroundColor Cyan
 
-# Vérifier l’état de certains services clés
+# Verifier l’etat de certains services cles
 Get-Service -Name NTDS, ADWS, DFSR, KDC, Netlogon, IsmServ | Format-Table Name, Status, DisplayName -AutoSize
