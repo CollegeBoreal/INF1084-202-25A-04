e360c0aa722795353875d2506f4b7588724b9600
makoudimassinissa <makoudimassimtts@gmail.com>
2025-12-09 16:56:15 -0500

    script

--- 4.OUs/300151354/bootstrap.ps1
+++ 4.OUs/300151354/bootstrap.ps1
@@ -21,15 +21,3 @@ $secure = ConvertTo-SecureString $plain -AsPlainText -Force
 # Credential administrateur
 $cred = New-Object System.Management.Automation.PSCredential("Administrator@$domainName", $secure)
 
-# Importer Active Directory
-Import-Module ActiveDirectory
-
-Write-Host "`nModule AD chargé avec succès.`n"
-
-# Vérification du domaine
-Write-Host "Vérification du domaine..."
-Get-ADDomain -Server $domainName
-
-Write-Host "`nContrôleurs de domaine :"
-Get-ADDomainController -Filter * -Server $domainName
-
