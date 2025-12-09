19f90e3b1d6174e9f6ff00dc226e8f0bd69d7272
AkremBouraoui <300150527@monboreal.ca>
2025-12-09 18:13:17 -0500

    Mise à jour

--- 4.OUs/300150527/bootstrap.ps1
+++ 4.OUs/300150527/bootstrap.ps1
@@ -11,5 +11,3 @@ $plain = 'Infra@2024'
 $secure = ConvertTo-SecureString $plain -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential("Administrator@$domainName", $secure)
 
-
-
